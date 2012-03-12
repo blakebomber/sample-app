9e623a4803b130cf5e932d0574138266a53e5286
Blake Bomber <blakebomber@gmail.com>
2012-03-11 22:47:05 -0600

    Finish user edit, update, index, and destroy actions

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Static pages" do
 
   subject { page }
-
+  
   shared_examples_for "all static pages" do
     it { should have_selector('h1',    text: heading) }
     it { should have_selector('title', text: full_title(page_title)) }
@@ -12,11 +12,12 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
     let(:heading)    { 'Sample App' }
-    let(:page_title) { 'Home' }
-
+    let(:page_title) { '' }
+    
     it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Home' }
   end
-  
+
   describe "Help page" do
     before { visit help_path }
     let(:heading)    { 'Help' }
@@ -27,7 +28,7 @@ describe "Static pages" do
 
   describe "About page" do
     before { visit about_path }
-    let(:heading)    { 'About Us' }
+    let(:heading)    { 'About' }
     let(:page_title) { 'About Us' }
 
     it_should_behave_like "all static pages"
@@ -50,7 +51,7 @@ describe "Static pages" do
     click_link "Contact"
     page.should have_selector 'title', text: full_title('Contact')
     click_link "Home"
-    page.should have_selector 'title', text: full_title('Home')
+    page.should_not have_selector 'title', text: full_title('Home')
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
   end
